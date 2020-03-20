2b72a1185101e0134f18c978c7d21a0916ceaf3c
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-20 11:13:41 -0700

    Added User model

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord 
+  #associations
+   has_many :user_comics
+   has_many :comicsofUser, :through => :user_comics, :source => 'comic'
+
+   has_many :reviews
+   has_many :comic_reviews, :through => :reviews, :source => 'comic'
+
+   has_many :comments
+   ##############################################################################
+   validates_presence_of :email
+   validates_uniqueness_of :email
+
+   def add_comic_to_user(comic)
+     if self.credits >= comic.price.to_i
+       self.comicsofUser << comic
+       self.credits -= comic.price.to_i
+       self
+     else
+       return nil
+     end
+   end
+end
