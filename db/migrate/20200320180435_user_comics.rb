345a1b263618922f7b2ba1898e5e3e3df64812e9
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-20 11:31:39 -0700

    Added carrierwave class uploader

--- db/migrate/20200320180435_user_comics.rb
+++ db/migrate/20200320180435_user_comics.rb
@@ -1,7 +1,9 @@
 class UserComics < ActiveRecord::Migration[5.0]
   def change
-    t.belongs_to :user
-    t.belongs_to :comic
-    t.timestamps
+    create_table :user_comics do |t|
+      t.belongs_to :user
+      t.belongs_to :comic
+      t.timestamps
+    end  
   end
 end
