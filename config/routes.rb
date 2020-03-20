cf8518052495c13c5fb5a2e1327c71e0b743f262
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-20 11:38:41 -0700

    Added routes for comics,publishers,characters

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :comics, only:[:index,:show]
+  resources :publishers, only:[:index,:show]
+  resources :characters, only:[:index,:show]
 end
