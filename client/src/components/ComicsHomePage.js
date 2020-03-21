2072167c30fd5032a863a2c21d8680bf1d79f4f2
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-21 13:59:18 -0700

    added character item and list component

--- client/src/components/ComicsHomePage.js
+++ client/src/components/ComicsHomePage.js
@@ -2,15 +2,18 @@ import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import ComicsList from './ComicsList';
+import PublishersList from './PublishersList';
+import CharactersList from './CharactersList';
 import { startSetComics } from '../actions/comics';
 import { startSetPublishers } from '../actions/publishers';
-import PublishersList from './PublishersList';
+import { startSetCharacters } from '../actions/characters';
 import { useSelector, useDispatch } from 'react-redux';
 
 const ComicsHomePage = () => {
     const dispatch = useDispatch();
     const comics = useSelector(state => state.comics.length > 3 ? state.comics.slice(0,3) : state.comics);
     const publishers = useSelector(state => state.publishers.length > 3 ? state.publishers.slice(0,3) : state.publishers);
+    const characters = useSelector(state => state.characters.length > 3 ? state.characters.slice(0,3) : state.characters);
 
     useEffect(() => {
       dispatch(startSetComics())
@@ -21,17 +24,23 @@ const ComicsHomePage = () => {
       .then(() => {
         console.log("Publisher fetched!");
       });
+      dispatch(startSetCharacters())
+      .then(() => {
+        console.log("Characters fetched!");
+      });
     },[dispatch]);
     return(
       <>
       <Container fluid>
         <h1>Latest Comics</h1>
-        <ComicsList comics={comics}/>
-        <NavLink to="/comics">See all Comics</NavLink>
+          <ComicsList comics={comics}/>
+          <NavLink to="/comics">See all Comics</NavLink>
         <h1>Publishers</h1>
-        <PublishersList publishers={publishers}/>
-        <NavLink to="/publishers">See all Publishers</NavLink>
+          <PublishersList publishers={publishers}/>
+          <NavLink to="/publishers">See all Publishers</NavLink>
         <h1>Characters</h1>
+          <CharactersList  characters={characters}/>
+          <NavLink to="/characters">See all Characters</NavLink>
       </Container>
       </>
   );
--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import comicsReducer from '../reducers/comics';
 import publishersReducer from '../reducers/publishers';
+import charactersReducer from '../reducers/characters';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -10,6 +11,7 @@ export default () => {
     combineReducers({
       comics: comicsReducer,
       publishers: publishersReducer,
+      characters: charactersReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
   );
