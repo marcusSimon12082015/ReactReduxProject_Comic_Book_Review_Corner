c6a9fe9263c708c4a123e5b7809f3190b2c53f4a
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-21 11:17:37 -0700

    added reducers to store

--- client/src/components/ComicsHomePage.js
+++ client/src/components/ComicsHomePage.js
@@ -2,17 +2,24 @@ import React, {useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import ComicsList from './ComicsList';
+import PublishersList from './ComicsList';
 import { startSetComics } from '../actions/comics';
+import { startSetPublishers } from '../actions/publishers';
 import { useSelector, useDispatch } from 'react-redux';
 
 const ComicsHomePage = () => {
   const dispatch = useDispatch();
   const comics = useSelector(state => state.comics.length > 3 ? state.comics.slice(0,3) : state.comics);
+  const publishers = useSelector(state => state.publishers.length > 3 ? state.publishers.slice(0,3) : state.publishers);
 
   useEffect(() => {
      dispatch(startSetComics())
      .then(() => {
        console.log("Comics fetched!");
+     });
+     dispatch(startSetPublishers())
+     .then(() => {
+       console.log("Publishers fetched!");
      });
    },[dispatch]);
   return(
@@ -20,8 +27,10 @@ const ComicsHomePage = () => {
       <Container fluid>
       <h1>Latest Comics</h1>
       <ComicsList comics={comics} />
-      <NavLink to="/comics">See all Comics</NavLink>
+        <NavLink to="/comics">See all Comics</NavLink>
       <h1>Publishers</h1>
+      <PublishersList publishers={publishers}/>
+      <NavLink to="/publishers">See all Publishers</NavLink>
       <h1>Characters</h1>
       </Container>
     </>
--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import comicsReducer from '../reducers/comics';
+import publishersReducer from '../reducers/publishers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -8,6 +9,7 @@ export default () => {
   const store = createStore(
     combineReducers({
       comics: comicsReducer,
+      publishers: publishersReducer,
     }),
     composeEnhancers(applyMiddleware(thunk))
   );
