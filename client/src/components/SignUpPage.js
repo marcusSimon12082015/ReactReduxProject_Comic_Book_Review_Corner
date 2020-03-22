1ad8109406040441c7cf5b3d91e93b5d4eb5ea8e
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 10:51:14 -0700

    Added needed imports for validation and form construction

--- client/src/components/SignUpPage.js
+++ client/src/components/SignUpPage.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { NavLink } from 'react-router-dom';
+import validator from 'validator';
+import { connect } from 'react-redux';
 import { startRegistration } from '../actions/auth';
 import { cleanRegistrationMessages } from '../actions/auth';
 
@@ -20,4 +25,13 @@ class SignUpPage extends React.Component {
   }
 }
 
-export default SignUpPage;
+const mapDispatchToProps = dispatch => ({
+  startRegistration: user => dispatch(startRegistration(user)),
+  cleanRegistrationMessages: () => dispatch(cleanRegistrationMessages())
+});
+
+const mapStateToProps = state => ({
+  registrationError: state.auth.messages.registrationError
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(SignUpPage);
