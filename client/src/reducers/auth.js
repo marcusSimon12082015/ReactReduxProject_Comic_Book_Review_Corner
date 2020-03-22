1ce7d15abb11642557dfbe1976bfe48bdb80c0ce
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 09:32:11 -0700

    added login and login failed cases to reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -9,3 +9,27 @@ const initialState = {
   },
   logged_in:false
 };
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        messages:{
+          ...state.messages,
+          loginError:''
+        },
+        user: action.payload,
+        logged_in:true
+      };
+    case 'LOGIN_FAILED':
+      return{
+        ...state,
+        messages:{
+          ...state.messages,
+          loginError:action.payload,
+        }
+      };
+      default:
+        return state;
+    }
+  };
