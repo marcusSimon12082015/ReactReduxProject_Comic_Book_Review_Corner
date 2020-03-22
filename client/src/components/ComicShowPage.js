50ef89970db506d79190001a91c8c8aaa054255d
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 12:18:52 -0700

    Added ComicShowPage conditional if comic not found

--- client/src/components/ComicShowPage.js
+++ client/src/components/ComicShowPage.js
@@ -17,7 +17,21 @@ const ComicShowPage = props => {
   const alreadyOwned = useSelector(state => state.auth.user.comicsofUser && state.auth.user.comicsofUser.some((comicUser) => comicUser.id === comicId));
 
   return(
-    <></>
+    <Container fluid>
+    {messages && <FlashMessage />}
+    {comic === undefined ? (
+      <Row>
+        <Col>
+          <div className="comic-show-container">
+            <h1>Comic Not Found</h1>
+          </div>
+        </Col>
+      </Row>
+      ):(
+        <></>
+      )
+    }
+    </Container>
   );
 }
 
