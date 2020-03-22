0441f050ae687ea0ac73b995c704f49788047ee4
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 10:59:57 -0700

    Added routes for registration and login and form structure

--- client/src/components/SignUpPage.js
+++ client/src/components/SignUpPage.js
@@ -17,10 +17,72 @@ class SignUpPage extends React.Component {
   componentWillUnmount(){
     this.props.cleanRegistrationMessages();
   }
-
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    let isValid = this.validateOnFormSubmit();
+    if (isValid) {
+      //validation passes send to server
+      console.log(this.state);
+      this.props.startRegistration({
+        email: this.state.email,
+        password: this.state.password,
+        password_confirmation: this.state.password_confirmation
+      }).then(() => {
+        if (!!this.props.registrationError) {
+          this.setState(() => ({registrationError:  this.props.registrationError}))
+        }else{
+          this.props.history.push("/");
+        }
+      })
+    }
+  }
+  validateOnFormSubmit = () => {
+    if (!this.state.email || !this.state.password || !this.state.password_confirmation) {
+      this.setState(() => ({registrationError: 'Please provide email and password.'}))
+      return false;
+    }else if(this.state.email && !validator.isEmail(this.state.email)){
+      this.setState(() => ({registrationError: 'Email format is invalid!'}))
+      return false;
+    }
+    if (this.state.error !== undefined) {
+      this.setState(() => ({ registrationError: '' }))
+    }
+    return true;
+  }
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]:e.target.value
+    })
+  }
   render(){
     return(
-      <div></div>
+      <div className="container_form">
+        <div className="box">
+        <h1>Registration Form!</h1>
+        <Form onSubmit={this.handleFormSubmit}>
+          {this.state.registrationError && <p className="error_class">{this.state.registrationError}</p>}
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" name="email"
+              onChange={this.onChange} />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" name="password"
+              onChange={this.onChange} />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password confirmation</Form.Label>
+            <Form.Control type="password" placeholder="Password confirmation" name="password_confirmation"
+              onChange={this.onChange} />
+          </Form.Group>
+          <Button variant="success" type="submit" size="large" block>
+            Sign Up
+          </Button>
+        </Form>
+        </div>
+        <NavLink to="/">Back To HomePage</NavLink>
+      </div>
     )
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :comics, only:[:index,:show]
   resources :publishers, only:[:index,:show]
   resources :characters, only:[:index,:show]
   resources :reviews, only:[:index,:show]
+
+  get 'user_comics/add_comic_to_user/:comic_id', to:'user_comics#add_comic_to_user', as:'add_comic_to_user'
+
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
 end
