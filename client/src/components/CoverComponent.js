56dbe5ed359a6e2c0f51fa0231f41028008fcb99
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 09:09:06 -0700

    added structure for CoverComponent

--- client/src/components/CoverComponent.js
+++ client/src/components/CoverComponent.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const CoverComponent = () => (
-  <>
-  </>
+    <div className="header-cover">
+    <div className="text-box">
+      <h1 className="heading-primary">
+        <span className="heading-primary_main">Join Our Community</span>
+        <span className="heading-primary_sub">Get free 200 credits on Sign Up</span>
+      </h1>
+      <NavLink to="/reviews" className="btn-white">
+        See our user reviews
+      </NavLink>
+    </div>
+  </div>
 );
+
+export default CoverComponent;
