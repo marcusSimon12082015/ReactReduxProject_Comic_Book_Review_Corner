17fc731e32993e8ac7b94e8fd8f69428a17f0e29
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 08:11:17 -0700

    Added SignUpPage to AppRouter

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PublicRoute from './PublicRoute';
 import ComicsHomePage from '../components/ComicsHomePage';
+import SignUpLayout from '../components/SignUpLayout';
+import MainLayout from '../components/MainLayout';
 import ReviewsPage from '../components/ReviewsPage';
+import SignUpPage from '../components/SignUpPage';
 import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
@@ -10,6 +13,7 @@ const AppRouter = () => (
     <div>
       <Switch>
         <PublicRoute path="/" component={ComicsHomePage} layout={MainLayout} exact={true} />
+        <PublicRoute path="/signup" component={SignUpPage} layout={SignUpLayout} />
         <PublicRoute path="/reviews" component={ReviewsPage} layout={MainLayout} />
         <PublicRoute component={NotFoundPage} layout={MainLayout}/>
       </Switch>
