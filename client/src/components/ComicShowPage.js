b9e7bfe5c20e8e31b79628fe70cdd6e94c839a72
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 12:00:38 -0700

    Added FlashMessage component

--- client/src/components/ComicShowPage.js
+++ client/src/components/ComicShowPage.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React,{ useEffect,useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { startAddComicToUser } from '../actions/auth';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import FlashMessage from './FlashMessage';
 
 
 const ComicShowPage = props => {
-  return(
+  const dispatch = useDispatch();
+  const comicId = parseInt(props.match.params.comicId);
+  const comic = useSelector(state => state.comics.find((comic) => comic.id === comicId));
+  const messages = useSelector(state => Object.values(state.auth.messages).some(key => key !== ""));
+  const isAuthenticated = useSelector(state => state.auth.logged_in);
+  const alreadyOwned = useSelector(state => state.auth.user.comicsofUser && state.auth.user.comicsofUser.some((comicUser) => comicUser.id === comicId));
 
+  return(
+    <></>
   );
 }
 
---
+++ client/src/components/FlashMessage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+
+const FlashMessage = props => {
+  return(
+    <></>
+  )
+}
+
+export default FlashMessage;
