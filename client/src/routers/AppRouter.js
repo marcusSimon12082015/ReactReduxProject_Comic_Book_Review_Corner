8d27007bd3991704c2d2017b4b46133f891d334f
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 07:55:26 -0700

    Added MainLayout component and changed Route to PublicRoute in AppRouter

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import PublicRoute from './PublicRoute';
 import ComicsHomePage from '../components/ComicsHomePage';
 import ReviewsPage from '../components/ReviewsPage';
 import NotFoundPage from '../components/NotFoundPage';
@@ -8,9 +9,9 @@ const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Switch>
-        <Route path="/" component={ComicsHomePage} exact={true} />
-        <Route path="/reviews" component={ReviewsPage} />
-        <Route component={NotFoundPage} />
+        <PublicRoute path="/" component={ComicsHomePage} layout={MainLayout} exact={true} />
+        <PublicRoute path="/reviews" component={ReviewsPage} layout={MainLayout} />
+        <PublicRoute component={NotFoundPage} layout={MainLayout}/>
       </Switch>
     </div>
   </BrowserRouter>
