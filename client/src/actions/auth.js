f615fc43097e561bc5994252de68ea9c611025bd
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 09:37:08 -0700

    added login status actions

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -30,3 +30,26 @@ export const loginFailed = (error) => ({
   type:'LOGIN_FAILED',
   payload:error
 });
+
+export const checkLoginStatus = () => {
+  return (dispatch) => {
+    return fetch("http://localhost:3001/logged_in",{
+      method:'GET',
+      credentials:'include',
+      headers:{
+        'Content-Type': 'application/json',
+         Accept: 'application/json'
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.logged_in) {
+        dispatch(login(data.user));
+      }
+    })
+  };
+}
+export const cleanLoginMessages = () => ({
+  type:'CLEAN_LOGIN_MESSAGES'
+});
+///////////////////////////////////////////////////////////////////////////////
