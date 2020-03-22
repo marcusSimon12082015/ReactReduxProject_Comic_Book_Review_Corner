f29a17ae45f616172af3dd7dc48747ad9eff58af
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 07:45:47 -0700

    Added login signup to Heade componet

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
 const Header = props => {
   return(
-
+    <header className="header">
+      <span><NavLink to="/">ComicBook Review</NavLink></span>
+      <ul>
+        <li>
+          <Button variant="top" size="lg">Login</Button>
+        </li>
+        <li>
+        <NavLink to="/signup">
+            <Button variant="danger" size="lg">Sign up</Button>
+        </NavLink>
+        </li>
+      </ul>
+    </header>
   );
 };
 
