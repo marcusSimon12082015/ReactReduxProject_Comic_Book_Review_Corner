cc30250d3c4016cc0e61ff8685d4a9f0226fa99a
Matija Simin <marcussimon12082015@hotmail.com>
2020-03-22 13:07:32 -0700

    Added ProfilePage component user and isAuthenticated flag

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import ComicsList from './ComicsList';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
 
 const ProfilePage = props => {
+  const isAuthenticated = useSelector(state => state.auth.logged_in);
+  const user = useSelector(state => state.auth.user);
     return(
-      <></>
+      <Container fluid style={{ backgroundColor: '#fbeec1' }}>
+
+      </Container>
     )
 };
 
